0ee450c77ad1f11d74e172dd2ecfe1dbe15981cc
Kamron Hays <kamron.hays@live.com>
2018-08-18 08:00:49 -0400

    Update javascript and ruby solutions

--- src/001/001.js
+++ src/001/001.js
@@ -1,17 +1,32 @@
+#!/usr/bin/env node
+
 // Find the sum of all the multiples of 3 or 5 below the specified value.
 // For example, if we list all the natural numbers below 10 that are multiples of 3 or 5,
 // we get 3, 5, 6 and 9. The sum of these multiples is 23.
-function sumMultiplesOf3and5(number)
+function sumMultiples(numbers, max)
 {
   var sum = 0;
-    
-  for (var i = 0; i < number; i++)
+
+  if ( numbers )
   {
-    if ( (i % 3 === 0) || (i % 5 === 0) )
+    for (var i = 1; i < max; i++)
     {
-      sum += i;
+      for (var j = 0; j < numbers.length; j++)
+      {
+        if ( i % numbers[j] == 0 )
+        {
+          sum += i;
+          break;
+        }
+      }
     }
   }
 
   return sum;
 }
+
+console.log( sumMultiples(null, 10) == 0 );
+console.log( sumMultiples([3], 10) == 18 );
+console.log( sumMultiples([5], 10) == 5 );
+console.log( sumMultiples([3,5], 10) == 23 );
+console.log( sumMultiples([3,5], 1000) == 233168 );
--- src/001/001.rb
+++ src/001/001.rb
@@ -5,15 +5,14 @@
 # are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 def sum_multiples(*numbers, max)
   sum = 0
-
-  numbers.each do |n|
-    multiple = n
-    while multiple < max
-      sum += multiple
-      multiple += n
+  (1...max).each do |i|
+    numbers.each do |n|
+      if i % n == 0
+        sum += i
+        break
+      end
     end
   end
-
   sum
 end
 
@@ -21,4 +20,4 @@ puts sum_multiples(10) == 0
 puts sum_multiples(3, 10) == 18
 puts sum_multiples(5, 10) == 5
 puts sum_multiples(3, 5, 10) == 23
-puts sum_multiples(3, 5, 1000)
+puts sum_multiples(3, 5, 1000) == 233168
