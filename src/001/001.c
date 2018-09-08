e647d9de0abaa6ccf79e31e483106f272ebbe2c2
Kamron Hays <kamron.hays@live.com>
2018-09-08 08:51:27 -0400

    Update Problem 1 C solution

--- src/001/001.c
+++ src/001/001.c
@@ -32,15 +32,17 @@ int SumMultiples(int *numbers, int size, int max)
     return sum;
 }
 
+void Test(int term) { printf("%s\n", term ? "true" : "false"); }
+
 int main(void)
 {
     int ary1[] = { 3 };
     int ary2[] = { 5 };
     int ary3[] = { 3, 5 };
-    printf("%s\n", SumMultiples(NULL, 0, 10) == 0 ? "pass" : "fail");
-    printf("%s\n", SumMultiples(ary1, 1, 10) == 18 ? "pass" : "fail");
-    printf("%s\n", SumMultiples(ary2, 1, 10) == 5 ? "pass" : "fail");
-    printf("%s\n", SumMultiples(ary3, 2, 10) == 23 ? "pass" : "fail");
-    printf("%s\n", SumMultiples(ary3, 2, 1000) == 233168 ? "pass" : "fail");
+    Test( SumMultiples(NULL, 0, 10) == 0 );
+    Test( SumMultiples(ary1, 1, 10) == 18 );
+    Test( SumMultiples(ary2, 1, 10) == 5 );
+    Test( SumMultiples(ary3, 2, 10) == 23 );
+    Test( SumMultiples(ary3, 2, 1000) == 233168 );
     return 0;
 }
